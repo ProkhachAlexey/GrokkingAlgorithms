6e770f4472c32a081d3a48ff73acf46e8bda94ed
Alexey Prokhach <asprokhach68@gmail.com>
2023-03-26 22:22:08 +0300

    Added part3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,17 @@
 import part1.BinarySearch
 import part2.SortingWithChoice
+import part3.Factorial
 
 fun main() {
 
     // part1
-//    println("index element is: ${BinarySearch().elementIndex(BinarySearch.myList, 5)}")
+    println("index element is: ${BinarySearch().elementIndex(BinarySearch.myList, 5)}")
 
-    //part2
-    println(SortingWithChoice().selectionSort(SortingWithChoice.myList))
+    // part2
+    println("Sorting with choice: ${SortingWithChoice().selectionSort(SortingWithChoice.myList)}")
+
+    // part3
+    val number = 5
+    println("Factorial $number = ${Factorial().fact(number)}")
 }
 
