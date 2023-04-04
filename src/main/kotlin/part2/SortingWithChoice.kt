637198cde1292a7d47a4b7a560d2aff70d1854eb
Alexey Prokhach <asprokhach68@gmail.com>
2023-04-04 07:40:59 +0300

    Refactor selector sorting

--- src/main/kotlin/part2/SortingWithChoice.kt
+++ src/main/kotlin/part2/SortingWithChoice.kt
@@ -1,32 +1,22 @@
 package part2
 
 class SortingWithChoice {
-    private fun findSmallest(arr: List<Int>): Int {
-        var smallest = arr[0]
-        for (i in (1 until arr.size)) {
-            if (arr[i] < smallest) {
-                smallest = arr[i]
-            }
+
+    fun selectionSort(arr: MutableList<Int>): List<Int> {
+        val resultList = mutableListOf<Int>()
+        for (item in 0 until arr.size) {
+            val smallest = arr.minOrNull()
+            smallest?.let { resultList.add(it) }
+            arr.remove(smallest)
         }
-        return smallest
+        return resultList
     }
 
-    fun selectionSort(arr: List<Int>): List<Int> {
-        val myList = mutableListOf<Int>()
-        for (item in arr) {
-            myList.add(item)
-        }
-        val newList = mutableListOf<Int>()
-        for (item in 0 until myList.size) {
-            val smallest = findSmallest(myList)
-            newList.add(smallest)
-            myList.remove(smallest)
-        }
-        return newList
+    fun swap(list: MutableList<Int>, firstIndex: Int, secondIndex: Int) {
+
     }
 
     companion object {
-        val myList = listOf(-5, 3, 20, 6, -10)
+        val myList = mutableListOf(-5, 3, 20, 6, -10)
     }
 }
-
